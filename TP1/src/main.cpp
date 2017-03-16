73ce37fbdc8fd512a6bedd08ba3b42d0d41c8f41
adem hadra <ademongamex@gmail.com>
2017-03-16 16:20:10 +0100

    Ca fonctionne. Je crois. Oui, bon, après, les trucs du genre : Les 8 dans les 0 ou les 6 à l'envers, on y peut rien.

--- TP1/src/main.cpp
+++ TP1/src/main.cpp
@@ -2,20 +2,35 @@
 #include "jpg.h"
 #include "mnist.h"
 
+float euclid(float* v1, float* v2){
+	float eucl=0;
+	for(int i=0; i<784; i++){
+		eucl+=(v1[i]-v2[i])*(v1[i]-v2[i]);
+	}
+	return eucl;
+}
 
-
-int main(int argc, char** argv)
+int main()
 {
-    if(argc < 3) {fprintf(stderr, "Please provide a mnist file\n"); exit(1); }
-    string path = argv[1];
-	string path_labels= argv[2]
 
-    float** data = read_mnist(path);
-	float* labels = read_labels(path_labels);
 
-    for(int i=0; i<60000; i++) {
-        printf("%u\n", i);
-        save_jpg(data[i], 28, 28, "%u/%04u.jpg", (int)labels[i], i);
+    float** images = read_mnist("train-images.idx3-ubyte");
+	float* labels = read_labels("train-labels.idx1-ubyte");
+	float** test_images = read_mnist("t10k-images.idx3-ubyte");
+
+    for(int i=0; i<10000; i++) {
+		float mine=-1;
+		int nn;
+        for(int j=0; j<10000; j++){
+		float eucl = euclid(test_images[i], images[j]);
+		if(eucl<mine || mine==-1){
+			mine=eucl;
+			nn=j;
+			}
+		}
+		int inf = labels[nn];
+		printf("%u\n", i);
+        save_jpg(test_images[i], 28, 28, "%u/%04u.jpg", inf, i);
     }
     return 0;
 }
