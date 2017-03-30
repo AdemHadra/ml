57a0003e98f15a2adc5f9019955c61b88114dddf
adem hadra <ademongamex@gmail.com>
2017-03-30 18:03:51 +0200

    Bon, bah...

--- TP1/src/main.cpp
+++ TP1/src/main.cpp
@@ -1,6 +1,7 @@
 
 #include "jpg.h"
 #include "mnist.h"
+#include<limits.h>
 
 float euclid(float* v1, float* v2){
 	float eucl=0;
@@ -10,6 +11,18 @@ float euclid(float* v1, float* v2){
 	return eucl;
 }
 
+int lin_class(float *w, float *x){
+	float d = 0;
+	for(int  i=0; i<784; i++){
+			d+= w[i] *x[i];
+	}
+	if(d>=0){
+		return 1;
+	}else{
+		return 0;
+	}
+}
+
 int main()
 {
 
@@ -17,20 +30,21 @@ int main()
     float** images = read_mnist("train-images.idx3-ubyte");
 	float* labels = read_labels("train-labels.idx1-ubyte");
 	float** test_images = read_mnist("t10k-images.idx3-ubyte");
-
+	float* test_labels = read_labels("t10k-labels.idx1-ubyte");
+	//float err=0;
+	float *w = new float[784];
+	for(int i =0; i<784;i++){
+		w[i]=(float)rand()*2/INT_MAX-1;
+	}
     for(int i=0; i<10000; i++) {
-		float mine=-1;
-		int nn;
-        for(int j=0; j<10000; j++){
-		float eucl = euclid(test_images[i], images[j]);
-		if(eucl<mine || mine==-1){
-			mine=eucl;
-			nn=j;
-			}
-		}
-		int inf = labels[nn];
+		int inf = lin_class(w, test_images[i]); ;
 		printf("%u\n", i);
         save_jpg(test_images[i], 28, 28, "%u/%04u.jpg", inf, i);
+		//if(inf != test_labels[i]){
+			//err++;
+		//}
+	//printf("Il y a %0.2f%% d'erreur. \n", (err*100)/i);
     }
+	//printf("Il y a %0.2f %% d'erreur. \n", err);
     return 0;
 }
